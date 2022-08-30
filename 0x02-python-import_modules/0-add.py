da01f5eb828f1847a0abd0734cf350bb5d739a7c
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-08-30 16:44:38 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	from add_0 import add
-	a, b = 1, 2
-	print(f"{a} + {b} = {add(a, b)}")
+    from add_0 import add
+    a, b, = 1, 2
+    value = add(a, b)
+    print(f"{a} + {b} = {value}")
