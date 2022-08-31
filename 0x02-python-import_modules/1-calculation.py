226defe44c92ea9b742acb3dedd219a8b787bd3a
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-08-30 17:18:19 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    import calculator_1 as c1
+
+    a = 10
+    b = 5
+
+    print(f"{a} + {b} = {c1.add(a, b)}")
+    print(f"{a} - {b} = {c1.sub(a, b)}")
+    print(f"{a} * {b} = {c1.mul(a, b)}")
+    print(f"{a} / {b} = {c1.div(a, b)}")
