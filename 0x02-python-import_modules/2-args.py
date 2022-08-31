f86febab1a781625f93aa12b877bb7c9f377781a
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-08-30 18:10:58 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
 
     n_args = len(sys.argv) - 1
-    if n_args > 1: 
+    if n_args > 1:
         print(f"{n_args} arguments:")
-        [print(f"{i}: {sys.argv[i]}") for i in range(1,n_args + 1)]
+        [print(f"{i}: {sys.argv[i]}") for i in range(1, n_args + 1)]
     elif n_args == 1:
         print(f"{n_args} argument:")
         print(f"1: {sys.argv[1]}")
