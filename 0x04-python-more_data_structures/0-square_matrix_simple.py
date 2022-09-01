4f6650dace416d316d5f950f9f40c776032ca29d
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-08-31 19:19:08 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[list(map(lambda x: x**2)) for matrix[i]] for i in matrix]
-
+    return [list(map(lambda x: x**2, matrix[i])) for i in range(len(matrix))]
