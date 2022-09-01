cd6a03b94f203fcb3f6145d0b90c34f754b0983a
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-08-31 20:29:28 -0700

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,3 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary.update({key: value})
     return a_dictionary
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-print(update_dictionary(a_dictionary, 'language', "Python"))
