1303ad8b4ea40b17d33d25356909f9604af30368
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-09-03 06:08:27 -0700

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(99):
-    if i < 10:
-        print("0{}".format(i), end=", ")
-    else:
-        print("{}".format(i), end=", ")
-print("99")
+    print("{:02d}".format(i), end=", ")
+print(99)
