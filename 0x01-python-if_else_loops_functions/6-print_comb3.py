6289ec1bbb452318d76b523625500277ad4b9d2c
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-09-03 07:08:58 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 number = 0
 inc = 2
 while number <= 89:
-    if number % 10 == 0 and number not in [0, 1]:
+    if number % 10 == 0 and number != 1:
         number += inc
         inc += 1
     print("{:02d}".format(number), end=", " if number != 89 else "\n")
