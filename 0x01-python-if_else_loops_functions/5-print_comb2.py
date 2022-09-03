194cd29cee1ef63b04883e0350e736d3d40cd22a
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-09-03 05:56:23 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print("0{}".format(i), sep=", ")
+        print("0{}".format(i), end=", ")
     else:
-        print("{}".format(i), sep=", ")
+        print("{}".format(i), end=", ")
