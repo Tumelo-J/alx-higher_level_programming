b5dfc6f21666208cd55856c37952e8558250fb31
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-09-15 18:31:02 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    for ch in str:
+    for i, ch in enumerate(str):
         if ord(ch) in range(97, 123):
-            ch = chr(ord(ch) - 32)
+            str[i] = chr(ord(ch) - 32)
+        else:
+            str[i] = str[i]
     print("{}\n".format(str))
