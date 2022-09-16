29db1e6243c1f6813f6d993cb139c8e16d4fd1fc
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-09-15 21:33:46 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 
 def fizzbuzz():
     for n in range(1, 101):
-        if not (n % 3 and n % 5):
+        if (n % 3 and n % 5):
             print("FizzBuzz", end=" ")
-        elif not n % 3:
+        elif n % 3:
             print("Fizz", end=" ")
-        elif not n % 5:
+        elif  n % 5:
             print("Buzz")
         else:
             print(n, end=" ")
