e2d47b3c49dd9b5ec2cad8b903b5594fc3380f87
Tumelo-Joel <TumeloJ.Mtsweni.com>
2022-09-20 03:18:12 -0700

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/python3
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self._width
+        self._height
+
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self._width = value
+
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self._height = value
