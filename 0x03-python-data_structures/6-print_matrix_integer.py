53e4aa177809fa5061a25357329efd5110f4befa
Tumelo-Joel <TumeloJ.Mtsweni.com>
2022-09-22 07:34:21 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,5 @@ def print_matrix_integer(matrix=[[]]):
     for vector in matrix:
         columns = len(vector)
         for i, number in enumerate(vector):
-            str_ = "{}".format(number)
+            str_ = "{:d}".format(number)
             print(str_, end="\n" if not (i + 1) % columns else " ")
