c113c8042f879b22d7af4ad433842eea702cf380
Tumelo-Joel <TumeloJ.Mtsweni.com>
2022-09-22 07:28:10 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
     for vector in matrix:
         columns = len(vector)
         for i, number in enumerate(vector):
