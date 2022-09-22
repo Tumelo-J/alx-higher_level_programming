6a4441cc1d893ceb24780966a49a49d4902d8fe3
Tumelo-Joel <TumeloJ.Mtsweni.com>
2022-09-22 02:58:22 -0700

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    strcp ="".join(["" if str.index(ch) == n else ch for ch in str])
+    strcp = "".join(["" if str.index(ch) == n else ch for ch in str])
     return strcp
