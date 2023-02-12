21a0cd72954dfdf1d319de99c3cddd45b39308f2
Tumelo <TumeloJ.Mtsweni@gmail.com>
2023-02-12 03:36:25 +0000

    1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 Class Rectangle {
-    constructor(w, h) {
-        this.width = w;
-        this.height = h;
+  constructor(w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 
 module.exports = Rectangle;
